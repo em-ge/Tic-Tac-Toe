4cc0e2542b86888e7610a65ff75dff54b539cbf2
Emil Gerlach <emil.j.gerlach@gmail.com>
2024-07-27 08:26:39 +0200

    help+exit

--- tictactoe.py
+++ tictactoe.py
@@ -7,11 +7,11 @@ example = "\nType a number between 1 to 9\n1 2 3\n4 5 6\n7 8 9\n\n> "
 
 
 def start():
-    print(Style.BRIGHT + "\n\nTicTacToe:\n" + Style.RESET_ALL)
+    print(Style.BRIGHT + "\n\nTIC TAC TOE:\n" + Style.RESET_ALL)
     time.sleep(1)
-    print("Type your number, where you want to set the x or o\n\n1 2 3\n4 5 6\n7 8 9")
+    print("Type your number, where you want to set the x or o in this Game Board:\n\n1 2 3\n4 5 6\n7 8 9")
     print(
-        "If you have your item (x or o) in a row, ... or ... you win!\nType 'help' for help\nLet's Go!"
+        "If you have your item (x or o) in a row, ... or ... you win!\n\nType 'help' for help;\nType 'exit' to exit this Game\n\nLet's Go!"
     )
 
 
@@ -23,6 +23,12 @@ def game_Human():
         print(f"It's the turn of {check_Item}.")
         number = input("\nType a number between 1 to 9\n> ")
 
+        if number == "help":
+            help()
+
+        if number == "exit":
+            exit()
+
         while True:
             if number == "help":
                 start()
@@ -30,6 +36,13 @@ def game_Human():
             if number.isalpha():
                 print("Please type a number and no letter!")
                 number = input("\nType a number between 1 to 9\n> ")
+
+                if number == "help":
+                    help()
+
+                if number == "exit":
+                    exit()
+
                 continue
 
             number = int(number)
@@ -37,6 +50,13 @@ def game_Human():
             if number >= 10:
                 print("Please type a number with a free Field!")
                 number = input("\nType a number between 1 to 9\n> ")
+
+                if number == "help":
+                    help()
+                
+                if number == "exit":
+                    exit()
+
                 continue
             if (
                 output[number - 1] == "\x1b[1mo\x1b[0m"
@@ -44,12 +64,19 @@ def game_Human():
             ):
                 print("Please select an empty Field")
                 number = input("\nType a number between 1 to 9\n> ")
+
+                if number == "help":
+                    help()
+
+                if number == "exit":
+                    exit()
+
                 continue
             break
 
         output[number - 1] = Style.BRIGHT + check_Item + Style.RESET_ALL
 
-        game = f"\n{output[0]}  {output[1]}  {output[2]}\n{output[3]}  {output[4]}  {output[5]}\n{output[6]}  {output[7]}  {output[8]}\n\n"
+        game = f"\n{output[0]} {output[1]} {output[2]}\n{output[3]} {output[4]} {output[5]}\n{output[6]} {output[7]} {output[8]}\n\n"
 
         print(game)
         time.sleep(1)
@@ -96,13 +123,18 @@ def game_Human():
 
 
 def game_AI():
-    opportunity_to_win = False
     output = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
     for i in range(9):
         print("It's your turn")
         check_Item = "x"
         number = input("\nType a number between 1 to 9\n> ")
+
+        if number == "help":
+            help()
+
+        if number == "exit":
+            exit()
 
         while True:
             if number == "help":
@@ -111,6 +143,13 @@ def game_AI():
             if number.isalpha():
                 print("Please type a number and no letter!")
                 number = input("\nType a number between 1 to 9\n> ")
+
+                if number == "help":
+                    help()
+
+                if number == "exit":
+                    exit()
+
                 continue
 
             number = int(number)
@@ -118,6 +157,13 @@ def game_AI():
             if number >= 10:
                 print("Please type a number with a free Field!")
                 number = input("\nType a number between 1 to 9\n> ")
+
+                if number == "help":
+                    help()
+                
+                if number == "exit":
+                    exit()
+
                 continue
             if (
                 output[number - 1] == "\x1b[1mo\x1b[0m"
@@ -125,12 +171,19 @@ def game_AI():
             ):
                 print("Please select an empty Field")
                 number = input("\nType a number between 1 to 9\n> ")
+
+                if number == "help":
+                    help()
+
+                if number == "exit":
+                    exit()
+
                 continue
             break
 
         output[number - 1] = Style.BRIGHT + check_Item + Style.RESET_ALL
 
-        game = f"\n{output[0]}  {output[1]}  {output[2]}\n{output[3]}  {output[4]}  {output[5]}\n{output[6]}  {output[7]}  {output[8]}\n\n"
+        game = f"\n{output[0]} {output[1]} {output[2]}\n{output[3]} {output[4]} {output[5]}\n{output[6]} {output[7]} {output[8]}\n\n"
 
         print(game)
         time.sleep(1)
@@ -138,32 +191,26 @@ def game_AI():
         if output[0] == output[1] == output[2]:
             time.sleep(1)
             print(f"{Style.BRIGHT}{check_Item} wins!{Style.RESET_ALL}")
-            opportunity_to_win = True
             break
         elif output[3] == output[4] == output[5]:
             time.sleep(1)
             print(f"{Style.BRIGHT}{check_Item} wins!{Style.RESET_ALL}")
-            opportunity_to_win = True
             break
         elif output[6] == output[7] == output[8]:
             time.sleep(1)
             print(f"{Style.BRIGHT}{check_Item} wins!{Style.RESET_ALL}")
-            opportunity_to_win = True
             break
         elif output[0] == output[3] == output[6]:
             time.sleep(1)
             print(f"{Style.BRIGHT}{check_Item} wins!{Style.RESET_ALL}")
-            opportunity_to_win = True
             break
         elif output[1] == output[4] == output[7]:
             time.sleep(1)
             print(f"{Style.BRIGHT}{check_Item} wins!{Style.RESET_ALL}")
-            opportunity_to_win = True
             break
         elif output[2] == output[5] == output[8]:
             time.sleep(1)
             print(f"{Style.BRIGHT}{check_Item} wins!{Style.RESET_ALL}")
-            opportunity_to_win = True
             break
         elif output[0] == output[4] == output[8]:
             time.sleep(1)
@@ -173,15 +220,11 @@ def game_AI():
         elif output[2] == output[4] == output[6]:
             time.sleep(1)
             print(f"{Style.BRIGHT}{check_Item} wins!{Style.RESET_ALL}")
-            opportunity_to_win = True
             break
 
         print(f"It's the turn of AI.")
         check_Item = "o"
         ai_choice = random.choice(output)
-
-        if opportunity_to_win == True:
-            pass
 
         while True:
             if ai_choice == "\x1b[1mo\x1b[0m" or ai_choice == "\x1b[1mx\x1b[0m":
@@ -240,6 +283,14 @@ def check_choice():
     choice = input(
         "Do you want to play aganst an AI or a human?\nType 'AI' to play aganst a AI or 'human' to play angainst a human.\n> "
     )
+
+    if choice == "help":
+        help()
+
+    if choice == "exit":
+        exit()
+    
+
     if choice == "AI":
         print("Ok, Let's Go!")
         game_AI()
@@ -252,6 +303,14 @@ def check_choice():
             choice = input(
                 "Do you want to play aganst an AI or a human?\nType 'AI' to play aganst a AI or 'human' to play angainst a human.\n> "
             )
+
+            if choice == "help":
+                help()
+
+            if choice == "exit":
+                exit()
+
+
             if choice == "AI":
                 print("Ok, Let's Go!")
                 game_AI()
@@ -260,6 +319,39 @@ def check_choice():
                 game_Human()
                 print("Ok, Let's Go!")
                 break
+
+def help():
+    print("You entered help.\nType 'continue' to continue this Game;\n")
+    print("Type 'exit' to exit this Game;\nType again 'help' to get help")
+    while True:
+        help_menu = input("> ")
+        if help_menu == "exit":
+            exit()
+        elif help_menu == "help":
+            print("""
+HELP MENU
+
+            
+
+Type 'exit' to exit this Game;
+Type 'continue' to continue this Game;
+""")
+        elif help_menu == "continue":
+            break
+        else:
+            print("Please type a suitable word.")
+
+
+def exit():
+    print("Do you really wanna exit this game?\nType 'exit' again to exitcor 'continue' to continue playing this game.")     
+    while True:
+        exit = input("> ") 
+        if exit == "exit":
+            sys.exit()
+        if exit == "continue":
+            break
+        else:
+            print("Please type a suitable answer!")
 
 
 while True:
